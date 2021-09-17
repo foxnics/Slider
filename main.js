c103c734ca848c1e8f65d1703dae7b00b22d5d2d
foxnics <85386283+foxnics@users.noreply.github.com>
2021-09-18 04:00:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+let flag = 2;
+
+function controller(x) {
+    flag = flag + x;
+    slideShow(flag);
+
+}
+
+slideShow(flag);
+
+function slideShow(num) {
+    let slide = document.getElementsByClassName('slide');
+
+    if (num == slide.length) {
+        flag = 0;
+        num = 0;
+    }
+    if (num < 0) {
+        flag = slide.length - 1;
+        num = slide.length - 1;
+    }
+
+    for (let y of slide) {
+        y.style.display = "none";
+    }
+    slide[num].style.display = "block";
+}
